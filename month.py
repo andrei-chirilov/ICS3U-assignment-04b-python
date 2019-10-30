a7be8f30f2dae4bf77b11b13a119792ffd832cdc
Ubuntu <ubuntu@ip-172-31-28-78.ec2.internal>
2019-10-30 01:53:42 +0000

    c

---
+++ month.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Created by: Andrei Chirilov
+# Created on: Oct 2019
+# This program detirmines how many days there are in a month
+
+
+def main():
+    month = input("Enter a month: ")
+    print("")
+
+    if month == "January" or month == "March" or month == "May" or month == \
+    "July" or month == "August" or month == "October" or month == "December":
+        print("31 days")
+    elif month == "April" or month == "June" or month == \
+    "September" or month == "November":
+        print("30 days")
+    elif month == "February":
+        print("28 days/29 on leap years")
+    else:
+        print("This is not a valid month")
+
+
+if __name__ == "__main__":
+    main()
